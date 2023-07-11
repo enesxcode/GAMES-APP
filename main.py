a9fdebea9f763da2f7746b7551d07efd63e95fa9
ages25 <ernest.40frimpong@gmail.com>
2023-07-11 10:16:34 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+
+#correct guess number
+guess_number=9
+user_input=int(input('Enter guess a number(1-20)'))
+if user_input==guess_number:
+    print('Guesser was correct')
+else:
+    print('Guesser was wrong')    
