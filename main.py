a4041afc6c66c208a99c8cdc844ca10b5cbd135d
ages25 <ernest.40frimpong@gmail.com>
2023-07-11 10:39:36 +0000

    i imported a random module to this file

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+     #importing random module
+import random
 
-#correct guess number
+     #Correct Guess Number
 guess_number=9
 user_input=int(input('Enter guess a number(1-20)'))
 if user_input==guess_number:
     print('Guesser was correct')
 else:
-    print('Guesser was wrong')    
+    print('Guesser was wrong')   
+
+# abc=random.randint(1, 10)
+# print(abc)
